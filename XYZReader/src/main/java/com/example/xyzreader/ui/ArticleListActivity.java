4a6cac575cb2073b9fd2e73992a6270ceb974fdd
Mateusz Widuch <matt.widuch@gmail.com>
2016-08-21 19:28:20 +0100

    feat: Add texture to main activity's toolbar
    The appearance of the toolbar was somewhat basic with just solid colour as background. It looks more vibrant after adding a texture to it.

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -7,13 +7,16 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.Loader;
 import android.database.Cursor;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.v4.app.ShareCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatImageButton;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -41,6 +44,10 @@ public class ArticleListActivity extends AppCompatActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
+
+        // Blends Toolbar's background texture with app's primary colour
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.getBackground().setColorFilter(ContextCompat.getColor(this, R.color.theme_primary), PorterDuff.Mode.MULTIPLY);
 
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
